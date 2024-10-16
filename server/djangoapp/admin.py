b1aec7b5980f0935dca46912ab5eb8b6ced946a9
Kevspecial <knwokike@gmail.com>
2024-10-16 06:48:27 -0400

    module 3

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -6,9 +6,19 @@ admin.site.register(CarMake)
 admin.site.register(CarModel)
 
 # CarModelInline class
-
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['car_make', 'name', 'type', 'year']  
+    list_display = ('name', 'car_make', 'type', 'year') 
+    list_filter = ['type', 'year']  
+    search_fields = ['name', 'car_make__name']  
+
 
 # CarMakeAdmin class with CarModelInline
-
-# Register models here
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]  
+    list_display = ('name', 'description')  
+    search_fields = ['name'] 
