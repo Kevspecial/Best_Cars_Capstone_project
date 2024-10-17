66a7527b3c176caf7c4f0ec17728db91d521337b
Kevspecial <knwokike@gmail.com>
2024-10-17 07:19:47 -0400

    mod 4

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from .restapis import get_request, analyze_review_sentiments, post_review
+
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -17,6 +17,7 @@ urlpatterns = [
     
 
     # path for dealer reviews view
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,6 +2,7 @@ import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
 import Dealers from './components/Dealers/Dealers'
 import PostReview from "./components/Dealers/PostReview"
+import Dealers from './components/Dealers/Dealers'
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
